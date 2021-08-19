89b081b9dcf00a3f48ac84dc0e275ec2074b4160
Christian Mendoza <jonathan.mendoza@bennu.cl>
2021-08-19 01:30:58 -0500

    read search adn list history

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
@@ -6,11 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent {
 
-  constructor() { }
+  constructor( private gifsService: GifsService ){}
 
-  ngOnInit(): void {
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  call(): void {
+
+    const valor = this.txtBuscar.nativeElement.value;
+
+    this.gifsService.buscarGifs(valor);
+    this.txtBuscar.nativeElement.value = "";
   }
 
 }
